f9175c4cfe761a158657139628006d5ed02013b3
tirta71 <tirta4132@gmail.com>
2024-05-24 18:15:25 +0700

    first commit

---
+++ src/ChatsPage.jsx
@@ -0,0 +1,17 @@
+/* eslint-disable react/prop-types */
+import { PrettyChatWindow } from "react-chat-engine-pretty";
+
+const ChatsPage = (props) => {
+  return (
+    <div style={{ height: "100vh" }}>
+      <PrettyChatWindow
+        projectId="b6408dc4-4d52-418f-97e4-88158bf93819"
+        username={props.user.username}
+        secret={props.user.secret}
+        style={{ height: "100%" }}
+      />
+    </div>
+  );
+};
+
+export default ChatsPage;
